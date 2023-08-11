OfekiAlm/nanopb

commit 480e1905c8248761f19b7e97a0b4e8346fb50871

Make sure REL_PATH is presented first
The first proto_path supplied is used as the root of the project so
preferably use user provided path

extra/FindNanopb.cmake
@@ -125,6 +125,11 @@ function(NANOPB_GENERATE_CPP SRCS HDRS)
   if(NOT NANOPB_GENERATE_CPP_UNPARSED_ARGUMENTS)
     return()
   endif()
+  set(NANOPB_OPTIONS_DIRS)
+
+  if(NANOPB_GENERATE_CPP_RELPATH)
+	  list(APPEND _nanopb_include_path "-I${NANOPB_GENERATE_CPP_RELPATH}")
+  endif()
 
   if(NANOPB_GENERATE_CPP_APPEND_PATH)
     # Create an include path for each file specified
@@ -135,10 +140,6 @@ function(NANOPB_GENERATE_CPP SRCS HDRS)
     endforeach()
   else()
     set(_nanopb_include_path "-I${CMAKE_CURRENT_SOURCE_DIR}")
-  endif()
-
-  if(NANOPB_GENERATE_CPP_RELPATH)
-    list(APPEND _nanopb_include_path "-I${NANOPB_GENERATE_CPP_RELPATH}")
   endif()
 
   if(DEFINED NANOPB_IMPORT_DIRS)
@@ -216,9 +217,6 @@ function(NANOPB_GENERATE_CPP SRCS HDRS)
     list(APPEND ${SRCS} "${CMAKE_CURRENT_BINARY_DIR}/${FIL_PATH_REL}/${FIL_WE}.pb.c")
     list(APPEND ${HDRS} "${CMAKE_CURRENT_BINARY_DIR}/${FIL_PATH_REL}/${FIL_WE}.pb.h")
 
-    set(NANOPB_PLUGIN_OPTIONS)
-    set(NANOPB_OPTIONS_DIRS)
-
     # If there an options file in the same working directory, set it as a dependency
     get_filename_component(ABS_OPT_FIL ${FIL_DIR}/${FIL_WE}.options ABSOLUTE)
     if(EXISTS ${ABS_OPT_FIL})
@@ -245,6 +243,7 @@ function(NANOPB_GENERATE_CPP SRCS HDRS)
         list(REMOVE_DUPLICATES NANOPB_OPTIONS_DIRS)
     endif()
 
+    set(NANOPB_PLUGIN_OPTIONS)
     foreach(options_path ${NANOPB_OPTIONS_DIRS})
         set(NANOPB_PLUGIN_OPTIONS "${NANOPB_PLUGIN_OPTIONS} -I${options_path}")
     endforeach()
@@ -270,8 +269,8 @@ function(NANOPB_GENERATE_CPP SRCS HDRS)
       OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${FIL_PATH_REL}/${FIL_WE}.pb.c"
              "${CMAKE_CURRENT_BINARY_DIR}/${FIL_PATH_REL}/${FIL_WE}.pb.h"
       COMMAND  ${PROTOBUF_PROTOC_EXECUTABLE}
-      ARGS -I${GENERATOR_PATH} -I${GENERATOR_CORE_DIR}
-           -I${CMAKE_CURRENT_BINARY_DIR} ${_nanopb_include_path}
+      ARGS ${_nanopb_include_path} -I${GENERATOR_PATH}
+           -I${GENERATOR_CORE_DIR} -I${CMAKE_CURRENT_BINARY_DIR}
            --plugin=protoc-gen-nanopb=${NANOPB_GENERATOR_PLUGIN}
            ${NANOPB_OPT_STRING}
            ${PROTOC_OPTIONS}

commit 2fec88bf9ea9ad4248bb8e9c9042eecdcd6cf68b

Fix first path passed to protoc
Fix relative path passed to protoc when using RELPATH but
NANOPB_GENERATE_CPP_APPEND_PATH not set. In this case the path provided
by RELPATH should be passed to protoc first instead of the current
source directory.

This was working before and broke with 480e190.

Co-authored-by: Guillaume Lager <g.lager@innoseis.com>

extra/FindNanopb.cmake
@@ -144,7 +144,7 @@ function(NANOPB_GENERATE_CPP SRCS HDRS)
       list(APPEND _nanopb_include_path "-I${ABS_PATH}")
     endforeach()
   else()
-    set(_nanopb_include_path "-I${CMAKE_CURRENT_SOURCE_DIR}")
+    list(APPEND _nanopb_include_path "-I${CMAKE_CURRENT_SOURCE_DIR}")
   endif()
 
   if(DEFINED NANOPB_IMPORT_DIRS)